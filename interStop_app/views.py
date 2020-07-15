59d708c8d1bf9c8346b1bd75b3752a1f250061fd
Ronaldo <ronaldo1103mtz@gmail.com>
2020-07-15 00:08:36 -0600

    Version 0.0.1

---
+++ interStop_app/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def test(request):
+    return render(request, "test/test.html")
+
+def modelo(request):
+    return render(request, "modelo.html")
+
+def formulario(request):
+    return render(request, "formulario.html")
