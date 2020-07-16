fbdad9d9988add829443f203589597c268af55b8
Cristian <sr17012@ues.edu.sv>
2020-07-16 13:11:43 -0600

    creacion de caso de uso Modificar

---
+++ Modificar/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def form_mod (request):
+    
+    return render(request, "modificar_form.html")
--- interStop/urls.py
+++ interStop/urls.py
@@ -19,12 +19,16 @@ from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #interStop_app
 from interStop_app import views
+#caso de uso modificar
+from Modificar.views import form_mod
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', views.test),
     path('modelo/', views.modelo),
     path('formulario/', views.formulario),
+    path('modificar/',form_mod )
 ]
 
 urlpatterns += staticfiles_urlpatterns()
